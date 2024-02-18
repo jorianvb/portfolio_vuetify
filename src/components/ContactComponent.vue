b6b670876578c04ea1f7328578f5cbd190152f50
Valérian Baillet <valerianbaillet@gmail.com>
2024-02-18 19:23:39 +0100

    Mise à jour de la page de contact

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,15 +1,33 @@
 <template>
   <v-container class="fill-height">
     <v-container>
+      <v-row justify="center">
+        <h1>N'hésitez pas à me contacter</h1>
+      </v-row>
       <v-row align="center">
         <v-col cols="12" align="center">
-          valerianbaillet@gmail.com <br/>
-          <v-btn href="https://github.com/jorianvb" icon="mdi-github" />
-          <v-btn
-            href="https://www.linkedin.com/in/val%C3%A9rian-baillet-047861108/"
-            icon="mdi-linkedin"
-          />
+          <v-btn variant="text" target="_blank" href="mailto:valerianbaillet@gmail.com">
+            Par mail : valerianbaillet@gmail.com
+          </v-btn>
         </v-col>
+      </v-row>
+      <v-row align="center">
+          <v-col align="center">
+
+          <p>Ou sur mes réseaux</p>
+          </v-col>
+
+      </v-row>
+      <v-row align="center">
+          <v-col align="center">
+            <v-btn href="https://github.com/jorianvb" target="_blank" icon="mdi-github" class='me-3'/>
+            <v-btn
+              target="_blank"
+              href="https://www.linkedin.com/in/val%C3%A9rian-baillet-047861108/"
+              icon="mdi-linkedin"
+            />
+          </v-col>
+
       </v-row>
     </v-container>
   </v-container>
